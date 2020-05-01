9bb58a8bc48f6224c68108eee11299c8acaa6f85
BOURDEL Laurent <laurent.bourdel@gmail.com>
2020-05-01 13:05:32 +0200

    Update PAC PHP SCript

--- domocan/www/php/CmdPAC.php
+++ domocan/www/php/CmdPAC.php
@@ -5,40 +5,43 @@ include_once('/var/www/domocan/www/conf/config.php');
 // http://192.168.0.20/domocan/www/php/CmdPAC.php?state=0
   $titre = 'Gestion Pompe a chaleur';
 
-  $StateToSet = $_GET["state"];
-
   include '/var/www/domocan/class/class.envoiTrame.php5';
   include '/var/www/domocan/class/class.gradateur.php5';
 
+  echo "Bonjour !\n";
+
+
+try {
+  $conn = new PDO("mysql:host=localhost;dbname=DomoBourdel", 'user', 'user');
+  $sql = 'SELECT * FROM Status_PAC';
+
+  $result_query = $conn->query($sql);
+  $row = $result_query->fetch(PDO::FETCH_ASSOC); // only first row (we have only one !!)
+  print_r($row);
+  $status_PAC = $row['bool_status_PAC'];
+  $counter = $row['counter'];
+  print 'Status PAC'.$status_PAC.' Counter: '.$counter;
+ //        print_r($row0['0']['bool_status_PAC']);
+//    foreach($conn->query($sql) as $row) {
+//        print_r($row);
+//        print_r($row['bool_status_PAC']);
+//    }
+}catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+  $StateToSet = $_GET["state"];
+  print 'StateToSet : '.$StateToSet;
+
   $CmdPAC = new gradateur();
-
-  /* CONNEXION A LA BDD */
-  mysql_connect(MYSQL_HOST, MYSQL_LOGIN, MYSQL_PWD);
-  mysql_select_db(MYSQL_DB);
-
- /* AFFICHAGE DE L'ETAT DE LA CHAUDIERE */
-  $retour = mysql_query("SELECT `valeur` FROM `" . TABLE_CHAUFFAGE_CLEF_TEMP . "` WHERE `clef` = 'PAC'");                                                                                                    
-  $row = mysql_fetch_array($retour);
-
-  $chaudiere=$row[0];
-  print "Etat chaudiere= " . $chaudiere ;
-
-  print " Etat à setter = " . $StateToSet;
-
-  $retour = mysql_query("SELECT `valeur` FROM `" . TABLE_CHAUFFAGE_CLEF_TEMP . "` WHERE `clef` = 'counter'");
-  $row = mysql_fetch_array($retour);
-
-  $counter = $row[0];
-  print " Compteur= " . $counter;
-
 
   $carte=0x0C;
   $sortie=0x0C;
   $consigne=0x32;
   $delai=0;
 
-
-  if( $StateToSet xor $chaudiere)
+  if( $StateToSet xor $status_PAC)
   {
     if ( $StateToSet==0 ){
       $consigne=0;
@@ -53,16 +56,20 @@ include_once('/var/www/domocan/www/conf/config.php');
     }
     $CmdPAC->allumer($carte, $sortie, hexdec($delai), $consigne );
 
-    $query_string = "UPDATE `" . TABLE_CHAUFFAGE_CLEF_TEMP . "` SET `valeur` = $chaudiere  WHERE `clef` = 'PAC';";
-    mysql_query($query_string);
+    $query_string = "UPDATE Status_PAC SET bool_status_PAC=$StateToSet WHERE 1";
+    $result_query = $conn->query($query_string);
 //    print $query_string;
 
     $counter =  $counter + 1;
-    $query_string = "UPDATE `" . TABLE_CHAUFFAGE_CLEF_TEMP . "` SET `valeur` = $counter  WHERE `clef` = 'counter';";
-    mysql_query($query_string);
+
+    $query_string = "UPDATE Status_PAC SET counter=$counter WHERE 1";
+    $result_query = $conn->query($query_string);
 //    print $query_string;
   }
 
+// Close
+    $sql = null;
+    $conn = null;
 
 
 /*
@@ -86,8 +93,8 @@ include_once('/var/www/domocan/www/conf/config.php');
  */
 
  /* FERMETURE SQL */
-  mysql_close();
+//  mysql_close();
 
-  print ' Nouvel etat chaudiere=' . $chaudiere;
+  print ' Nouvel etat chaudiere=' . $StateToSet;
 
 ?>
